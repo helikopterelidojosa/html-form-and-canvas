531112b8678731e9a2079e2fa210479312b6b4a3
codesoup <82330389+helikopterelidojosa@users.noreply.github.com>
2022-07-06 05:40:27 +0300

    added coments to code

--- app.js
+++ app.js
@@ -1,41 +1,41 @@
+//next line updates changes live as soon as user inputs changes
 window.setInterval(()=>{
+
+    //next line determines that we take data from the html-Form 
     var myForm = document.getElementById('myForm');
-    // var text = document.getElementById('mc');
 
 
 
+//next lines sets up the canvas
     var canvas = document.getElementById('mC');
     var ctx = canvas.getContext('2d');
 
+    //next lines inputs the user's form data such as width and height
     canvas.width = myForm.elements['input-width'].value;
     canvas.height = myForm.elements['input-height'].value;
 
 
    
 
-
+// default circle color is black
+// but most probably user will make changes them
     var changeColor = myForm.elements['input-circle-color'].value;
     var circleColor = "" + changeColor;
     
 
 
-
+//next goes location of the circle in the canvas, on x and y axis
 var x = myForm.elements['input-circle-x'].value;
 var y = myForm.elements['input-circle-y'].value;
+
+//next goes size of the circle, by setting its respective radius
 var radius = myForm.elements['input-circle-radius'].value;
 
- 
+//next goes actually if the circle appears on the canvas 
 if(document.getElementById("setCircle").checked === true){
     ctx.arc(x, y, radius, 0, Math.PI * 2);
     ctx.fillStyle =circleColor;
     ctx.fill();
-}
+}; // here goes end of if.. statement  
 
-
-    
-    
-
-    
-
-
-}, 1000);
+}, 1000);  //here goes the end of the Interval, line 2
